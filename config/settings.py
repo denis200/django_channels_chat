a7a1b410c3658b94e0443ed6fbfd736a1f29fd3b
Denis Komarkovu <soundze@mail.ru>
2022-05-26 22:09:23 +0300

    Feat: Add ChatRoom Template| Add Chat logic

--- config/settings.py
+++ config/settings.py
@@ -116,3 +116,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ASGI_APPLICATION = 'config.routing.application'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
